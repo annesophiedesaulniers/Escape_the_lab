c3a6a25e7d80b8d75ade64d705cb8f2ede5a058c
annesophiedesaulniers <2372588@edu.vaniercollege.qc.ca>
2025-03-26 18:27:13 -0400

    cleaned the code

--- space-invaders/src/main/java/com/example/escape_the_lab/controller/AcidNeutralizationLab.java
+++ space-invaders/src/main/java/com/example/escape_the_lab/controller/AcidNeutralizationLab.java
@@ -8,9 +8,7 @@ package com.example.escape_the_lab.controller;
  *
  * @author ALAllaire
  */
-
 import com.example.escape_the_lab.model.Lab;
-import javafx.application.Platform;
 import com.example.escape_the_lab.model.Substance;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -31,20 +29,17 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
 import java.util.Objects;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class AcidNeutralizationLab extends Lab {
-    private String acid;
-    private String base;
+
     public ArrayList<Substance> activeSubstances = new ArrayList<>();
     public ArrayList<Substance> droppedSubstances = new ArrayList<>();
     @FXML
-    ImageView AcidSprite1, AcidSprite2,AcidSprite3,AcidSprite4,AcidSprite5, AcidImage1, AcidImage2, AcidImage3, AcidImage4, AcidImage5;    
+    ImageView AcidSprite1, AcidSprite2, AcidSprite3, AcidSprite4, AcidSprite5, AcidImage1, AcidImage2, AcidImage3, AcidImage4, AcidImage5;
     @FXML
     StackPane AcidHome1, AcidHome2, AcidHome3, AcidHome4, AcidHome5, AcidDisplay1, AcidDisplay2, AcidDisplay3, AcidDisplay4, AcidDisplay5;
     @FXML
@@ -53,111 +48,105 @@ public class AcidNeutralizationLab extends Lab {
     VBox AcidBank;
     private Lab lab;
     Stage primaryStage;
-     Substance substance1;
-     Substance substance2;
-     Substance substance3;
-     Substance substance4;
-     Substance substance5;
-    
-    
-    
+    Substance substance1;
+    Substance substance2;
+    Substance substance3;
+    Substance substance4;
+    Substance substance5;
+
     public AcidNeutralizationLab(Stage stage) {
         this.primaryStage = stage;
     }
 
-
-
-   @FXML
-private void initialize() {
-       initializeLab(lab);
-    AcidSprite1.setOpacity(0);
+    @FXML
+    private void initialize() {
+        initializeLab(lab);
+        AcidSprite1.setOpacity(0);
         AcidSprite2.setOpacity(0);
         AcidSprite3.setOpacity(0);
         AcidSprite4.setOpacity(0);
         AcidSprite5.setOpacity(0);
         setupDrag();
         setTarget();
-    if (AcidBank == null) {
-        System.out.println("arenaPane is still null!");
+        if (AcidBank == null) {
+            System.out.println("arenaPane is still null!");
+        }
+        for (Substance s : activeSubstances) {
+            System.out.println("Initialized: " + s.getName());
+        }
     }
-    for (Substance s : activeSubstances) {
-    System.out.println("Initialized: " + s.getName());
-}
-}
 
     @Override
     public void startLab() {
         primaryStage.setScene(createScene());
-    primaryStage.show();
+        primaryStage.show();
 
     }
 
     @Override
     public boolean checkSolution() {
         if (droppedSubstances.isEmpty()) {
-        return false; 
-    }
-        boolean hasNeutralized = false;
-        showFailedScreen();
- 
-
-    for (Substance substance : droppedSubstances) {
-        if (substance.equals(substance3) && substance.equals(substance5)) {
-            hasNeutralized = true;
+            return false;
         }
-      detectDroppedSubstances();
-    }
+        boolean hasNeutralized = false;
+        detectDroppedSubstances();
 
-    return hasNeutralized;
+        for (Substance substance : droppedSubstances) {
+            if (substance.equals(substance3) && substance.equals(substance5)) {
+                hasNeutralized = true;
+            }
+            detectDroppedSubstances();
+        }
+
+        return hasNeutralized;
     }
 
     @Override
     public void failLab() {
-
+showFailedScreen();
     }
 
     @Override
     public void setupLab() {
-
+createScene();
     }
- 
 
     @Override
     public Scene createScene() {
 
         try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AcidNeutralizationLab_layout.fxml"));
-        loader.setController(this);
-        Parent labRoot = loader.load();
-        return new Scene(labRoot, 1000, 650);
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    return null;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AcidNeutralizationLab_layout.fxml"));
+            loader.setController(this);
+            Parent labRoot = loader.load();
+            return new Scene(labRoot, 1000, 650);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void initializeLab(Lab lab) {
         this.lab = lab;
         substance1 = new Substance("Hydrochloric Acid", 1.0, AcidSprite1, 100, 100);
-     substance2 = new Substance("Sulfuric Acid", 2.0, AcidSprite2, 100, 100);
-     substance3 = new Substance("Sodium Hydroxide", 13.0, AcidSprite3, 100, 100);
-     substance4 = new Substance("Ammonia", 11.0, AcidSprite4, 100, 100);
-     substance5 = new Substance("Acetic Acid", 4.7, AcidSprite5, 100, 100);
+        substance2 = new Substance("Sulfuric Acid", 2.0, AcidSprite2, 100, 100);
+        substance3 = new Substance("Sodium Hydroxide", 13.0, AcidSprite3, 100, 100);
+        substance4 = new Substance("Ammonia", 11.0, AcidSprite4, 100, 100);
+        substance5 = new Substance("Acetic Acid", 4.7, AcidSprite5, 100, 100);
 
-    activeSubstances.add(substance1);
-    activeSubstances.add(substance2);
-    activeSubstances.add(substance3);
-    activeSubstances.add(substance4);
-    activeSubstances.add(substance5);
-  
+        activeSubstances.add(substance1);
+        activeSubstances.add(substance2);
+        activeSubstances.add(substance3);
+        activeSubstances.add(substance4);
+        activeSubstances.add(substance5);
+
     }
-     
+
     /**
      * Set up draggable substances from the VBox
      */
     private void setupDrag() {
- StackPane[] substances = {AcidHome1, AcidHome2, AcidHome3, AcidHome4, AcidHome5};
- //StackPane[] substances = {AcidDisplay1, AcidDisplay2, AcidDisplay3, AcidDisplay4, AcidDisplay5};
+        StackPane[] substances = {AcidHome1, AcidHome2, AcidHome3, AcidHome4, AcidHome5};
+        //StackPane[] substances = {AcidDisplay1, AcidDisplay2, AcidDisplay3, AcidDisplay4, AcidDisplay5};
         for (StackPane substance : substances) {
             ImageView img = new ImageView();
             for (Node child : substance.getChildren()) {
@@ -202,6 +191,7 @@ private void initialize() {
 //    /**
 //     * Set up the arena to accept dropped subtances
 //     */
+
     private void setTarget() {
         arenaPane.setOnDragOver(event -> {
             if (event.getGestureSource() != arenaPane && event.getDragboard().hasImage()) {
@@ -217,14 +207,14 @@ private void initialize() {
 //     * @param event The DragEvent from the drag and drop action of the user
 //     * moving the substance
 //     */
+
     private void addSubstanceToArena(DragEvent event) {
         Dragboard db = event.getDragboard();
 
-    if (db.hasImage()) {
-        StackPane draggedDisplay = (StackPane) event.getGestureSource();
+        if (db.hasImage()) {
+            StackPane draggedDisplay = (StackPane) event.getGestureSource();
 
-        Substance currentSubstance = createNewSubstance(draggedDisplay);
-        
+            Substance currentSubstance = createNewSubstance(draggedDisplay);
 
             try {
                 // Move the duck to the arena
@@ -245,25 +235,23 @@ private void initialize() {
                     arenaPane.getChildren().add(currentSubstance.display);
                     currentSubstance.sprite.setOpacity(1);
                     droppedSubstances.add(currentSubstance);
-                    
+
                 }
 
                 // make the duck draggable within the arena
                 Draggable(currentSubstance);
-                
-             //   detectDroppedSubstances();
 
+                //   detectDroppedSubstances();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-detectDroppedSubstances();
+            detectDroppedSubstances();
             event.setDropCompleted(true);
         } else {
             event.setDropCompleted(false);
         }
 
     }
-
 
 //    /**
 //     * Sets up the substance and substance bank to handle the substance being dragged back to
@@ -305,6 +293,7 @@ detectDroppedSubstances();
 //     *
 //     * @param substance selected substance
 //     */
+
     private void removeSubstance(Substance substance) {
         substance.display.setOpacity(0);
         substance.sprite.setFitWidth(100);
@@ -312,17 +301,16 @@ detectDroppedSubstances();
 
         switch (substance.substanceNumber()) {
             case 1 ->
-                    AcidHome1.getChildren().addLast(substance.display);
+                AcidHome1.getChildren().addLast(substance.display);
             case 2 ->
-                    AcidHome2.getChildren().addLast(substance.display);
+                AcidHome2.getChildren().addLast(substance.display);
             case 3 ->
-                    AcidHome3.getChildren().addLast(substance.display);
+                AcidHome3.getChildren().addLast(substance.display);
             case 4 ->
-                    AcidHome4.getChildren().addLast(substance.display);
+                AcidHome4.getChildren().addLast(substance.display);
             case 5 ->
-                    AcidHome5.getChildren().addLast(substance.display);
-        
-       
+                AcidHome5.getChildren().addLast(substance.display);
+
         }
 
         Draggable(substance);
@@ -336,9 +324,10 @@ detectDroppedSubstances();
 //     * @param substanceDisplay the StackPane to be assigned to the new Substance
 //     * @return the new Substance object
 //     */
+
     private Substance createNewSubstance(StackPane substanceDisplay) {
 
-Substance currentSusbtance = null;
+        Substance currentSusbtance = null;
         ImageView substanceImage = new ImageView();
         for (Node node : substanceDisplay.getChildren()) {
             if (node instanceof ImageView) {
@@ -346,23 +335,33 @@ Substance currentSusbtance = null;
             }
         }
 
-if (!Substance.existsIn(activeSubstances, substanceImage)) {
-    Substance newSubstance = new Substance(substanceImage);
-    switch (newSubstance.substanceNumber()) {
-        case 1 -> newSubstance = substance1;
-            case 2 -> newSubstance = substance2;
-                case 3 -> newSubstance = substance3;
-                    case 4 -> newSubstance = substance4;
-                        case 5 -> newSubstance = substance5;
-                         
-    }
-    switch (newSubstance.substanceNumber()) {
-        case 1 -> newSubstance.setHome(AcidSprite1);
-        case 2 -> newSubstance.setHome(AcidSprite2);
-        case 3 -> newSubstance.setHome(AcidSprite3);
-        case 4 -> newSubstance.setHome(AcidSprite4);
-        case 5 -> newSubstance.setHome(AcidSprite5);
-    }
+        if (!Substance.existsIn(activeSubstances, substanceImage)) {
+            Substance newSubstance = new Substance(substanceImage);
+            switch (newSubstance.substanceNumber()) {
+                case 1 ->
+                    newSubstance = substance1;
+                case 2 ->
+                    newSubstance = substance2;
+                case 3 ->
+                    newSubstance = substance3;
+                case 4 ->
+                    newSubstance = substance4;
+                case 5 ->
+                    newSubstance = substance5;
+
+            }
+            switch (newSubstance.substanceNumber()) {
+                case 1 ->
+                    newSubstance.setHome(AcidSprite1);
+                case 2 ->
+                    newSubstance.setHome(AcidSprite2);
+                case 3 ->
+                    newSubstance.setHome(AcidSprite3);
+                case 4 ->
+                    newSubstance.setHome(AcidSprite4);
+                case 5 ->
+                    newSubstance.setHome(AcidSprite5);
+            }
 
 //switch (newSubstance.substanceNumber()) {
 //    case 1:
@@ -385,20 +384,18 @@ if (!Substance.existsIn(activeSubstances, substanceImage)) {
                 other.setFocused(false);
             }
 
-          
             newSubstance.setFocused(true);
-          
 
             // Handle focus of ducks
             newSubstance.display.pressedProperty().addListener(event -> {
-        
-            for (Substance other : activeSubstances) {
-                other.setFocused(false);
-            }
-            // Set the duck's focused properties accordingly
-        //  newSubstance.setFocused(true);
-       
-    });
+
+                for (Substance other : activeSubstances) {
+                    other.setFocused(false);
+                }
+                // Set the duck's focused properties accordingly
+                //  newSubstance.setFocused(true);
+
+            });
 
             activeSubstances.add(newSubstance);
 
@@ -412,15 +409,12 @@ if (!Substance.existsIn(activeSubstances, substanceImage)) {
         }
         return currentSusbtance;
 
-        
-}
-
-    
+    }
 
     private void detectDroppedSubstances() {
         for (Substance substance : droppedSubstances) {
-        System.out.println("Dropped substance: " + substance.getName());
-    }
+            System.out.println("Dropped substance: " + substance.getName());
+        }
         System.out.println("Dropped substances: " + droppedSubstances);
         if (droppedSubstances.contains(substance3) && droppedSubstances.contains(substance5)) {
             System.out.println("Success! You neutralized the floor");
@@ -480,49 +474,48 @@ if (!Substance.existsIn(activeSubstances, substanceImage)) {
 //    }
 
     private void showSuccessScreen() {
-    Stage successStage = new Stage();
-    successStage.setTitle("Lab Complete!");
+        Stage successStage = new Stage();
+        successStage.setTitle("Lab Complete!");
 
-    Label successLabel = new Label("You successfully neutralized the solution!\n\n "
-            + "Hydrochloric acid (HCl) was neutralized by two different bases: \n"
-            + "NaOH and CaCO₃. NaOH reacted in a typical acid-base reaction"
-            + "to form salt and water,\n while CaCO₃ created a bubbling effect by releasing CO₂ gas.");
-    successLabel.setStyle("-fx-font-size: 16px;");
-successLabel.setWrapText(true); // Allow automatic text wrapping
-successLabel.setMaxWidth(450);
-    Button closeButton = new Button("OK");
-    closeButton.setOnAction(e -> successStage.close());
+        Label successLabel = new Label("You successfully neutralized the solution!\n\n "
+                + "Hydrochloric acid (HCl) was neutralized by two different bases: \n"
+                + "NaOH and CaCO₃. NaOH reacted in a typical acid-base reaction"
+                + "to form salt and water,\n while CaCO₃ created a bubbling effect by releasing CO₂ gas.");
+        successLabel.setStyle("-fx-font-size: 16px;");
+        successLabel.setWrapText(true); // Allow automatic text wrapping
+        successLabel.setMaxWidth(450);
+        Button closeButton = new Button("OK");
+        closeButton.setOnAction(e -> successStage.close());
 
-    VBox layout = new VBox(10, successLabel, closeButton);
-    layout.setAlignment(Pos.CENTER);
-    layout.setPadding(new Insets(20));
+        VBox layout = new VBox(10, successLabel, closeButton);
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(20));
 
-    Scene successScene = new Scene(layout, 500, 300);
-    successStage.setScene(successScene);
-    successStage.show();
+        Scene successScene = new Scene(layout, 500, 300);
+        successStage.setScene(successScene);
+        successStage.show();
     }
-    
+
     private void showFailedScreen() {
-    Stage failedStage = new Stage();
-    failedStage.setTitle("Lab Failed!");
+        Stage failedStage = new Stage();
+        failedStage.setTitle("Lab Failed!");
 
-    Label failedLabel = new Label("You have mixed the wrong substances!\n You just "
-            + "lost a life. \n Hint: This is a special type of neutralization! Since "
-            + "HCl is a strong acid, it requires two different bases to be fully neutralized");
-    failedLabel.setStyle("-fx-font-size: 16px;");
-failedLabel.setWrapText(true); // Allow automatic text wrapping
-failedLabel.setMaxWidth(450);
-    Button closeButton = new Button("OK");
-    closeButton.setOnAction(e -> failedStage.close());
+        Label failedLabel = new Label("You have mixed the wrong substances!\n You just "
+                + "lost a life. \n Hint: This is a special type of neutralization! Since "
+                + "HCl is a strong acid, it requires two different bases to be fully neutralized");
+        failedLabel.setStyle("-fx-font-size: 16px;");
+        failedLabel.setWrapText(true); // Allow automatic text wrapping
+        failedLabel.setMaxWidth(450);
+        Button closeButton = new Button("OK");
+        closeButton.setOnAction(e -> failedStage.close());
 
-    VBox layout = new VBox(10, failedLabel, closeButton);
-    layout.setAlignment(Pos.CENTER);
-    layout.setPadding(new Insets(20));
+        VBox layout = new VBox(10, failedLabel, closeButton);
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(20));
 
-    Scene successScene = new Scene(layout, 500, 300);
-    failedStage.setScene(successScene);
-    failedStage.show();
+        Scene successScene = new Scene(layout, 500, 300);
+        failedStage.setScene(successScene);
+        failedStage.show();
     }
-    
-}
 
+}
